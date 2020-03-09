da2be5ee8757ed4f8f5823ceb1d9194101d8b0fe
vdias38 <victor.dias@meumobi.com>
2020-03-09 15:14:57 +0000

    add firebase SDK and initialize it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    AngularFireAuth,
   ],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA5Xvv-O_G531RILC50FlRBSWr-HVzlEJA',
+    authDomain: 'meu-starter.firebaseapp.com',
+    databaseURL: 'https://meu-starter.firebaseio.com',
+    projectId: 'meu-starter',
+    storageBucket: 'meu-starter.appspot.com',
+    messagingSenderId: '581248963506',
+    appId: '1:581248963506:web:f6ade1a647740bf0ee4aab',
+    measurementId: 'G-F9SXGF1EWK'
+  }
 };
 
 /*
